b0627827843ac86b2c16f2b467f57d6efbba2808
Havoc Pennington <hp@pobox.com>
2020-07-14 14:05:01 -0400

    Don't cut-and-paste big list of default attribute names in spec

--- spec/serializers/project_serializer_spec.rb
+++ spec/serializers/project_serializer_spec.rb
@@ -4,35 +4,35 @@ describe ProjectSerializer do
   context "without updated_at" do
     subject { described_class.new(build(:project)) }
 
+    DEFAULT_ATTRIBUTE_NAMES = %i[
+      dependent_repos_count
+      dependents_count
+      deprecation_reason
+      description
+      forks
+      homepage
+      keywords
+      language
+      latest_download_url
+      latest_release_number
+      latest_release_published_at
+      latest_stable_release
+      latest_stable_release_number
+      latest_stable_release_published_at
+      license_normalized
+      licenses
+      name
+      normalized_licenses
+      package_manager_url
+      platform
+      rank
+      repository_url
+      stars
+      status
+    ]
+
     it 'should have expected attribute names' do
-      expect(subject.attributes.keys).to eq(
-        %i[
-        dependent_repos_count
-        dependents_count
-        deprecation_reason
-        description
-        forks
-        homepage
-        keywords
-        language
-        latest_download_url
-        latest_release_number
-        latest_release_published_at
-        latest_stable_release
-        latest_stable_release_number
-        latest_stable_release_published_at
-        license_normalized
-        licenses
-        name
-        normalized_licenses
-        package_manager_url
-        platform
-        rank
-        repository_url
-        stars
-        status
-      ]
-                                         )
+      expect(subject.attributes.keys).to eq(DEFAULT_ATTRIBUTE_NAMES)
     end
   end
 
@@ -40,35 +40,7 @@ describe ProjectSerializer do
     subject { described_class.new(build(:project), show_updated_at: true) }
 
     it 'should have expected attribute names' do
-      expect(subject.attributes.keys).to eq(
-        %i[
-        dependent_repos_count
-        dependents_count
-        deprecation_reason
-        description
-        forks
-        homepage
-        keywords
-        language
-        latest_download_url
-        latest_release_number
-        latest_release_published_at
-        latest_stable_release
-        latest_stable_release_number
-        latest_stable_release_published_at
-        license_normalized
-        licenses
-        name
-        normalized_licenses
-        package_manager_url
-        platform
-        rank
-        repository_url
-        stars
-        status
-        updated_at
-      ]
-                                         )
+      expect(subject.attributes.keys).to eq(DEFAULT_ATTRIBUTE_NAMES + %i[updated_at])
     end
   end
 end
